f66dab9eea007afa19678531c4d5de8c158ee8bd
richiealford <richie.alford@gmail.com>
2019-02-12 15:30:45 -0800

    changed book to stateless component

--- src/Book.js
+++ src/Book.js
@@ -2,38 +2,40 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import Thumbnail from './Thumbnail'
 
-class Book extends Component {
+const Book = (props) => {
+
+  const handleChange = (selectedOption) => {
+    this.props.callbackParent(this.props.book, selectedOption.target.value);
+  }
 
   static propTypes = {
     book: PropTypes.object.isRequired,
   }
 
-  handleChange = (selectedOption) => {
-    this.props.callbackParent(this.props.book, selectedOption.target.value);
-  }
-
-  render() {
-    const { book } = this.props
-    return (
-      <div className="book">
-        <div className="book-top">
-          <Thumbnail book={book} />
-          <div className="book-shelf-changer">
-            <select onChange={this.handleChange} value={this.props.shelf} >
-              <option value="move" disabled>Move to...</option>
-              <option value="currentlyReading">Currently Reading</option>
-              <option value="wantToRead">Want to Read</option>
-              <option value="read">Read</option>
-              <option value="none">None</option>
-            </select>
-          </div>
+  const { book } = props
+  return (
+    <div className="book">
+      <div className="book-top">
+        <Thumbnail book={book} />
+        <div className="book-shelf-changer">
+          <select onChange={this.handleChange} value={this.props.shelf} >
+            <option value="move" disabled>Move to...</option>
+            <option value="currentlyReading">Currently Reading</option>
+            <option value="wantToRead">Want to Read</option>
+            <option value="read">Read</option>
+            <option value="none">None</option>
+          </select>
         </div>
-        <div className="book-title">{book.title}</div>
-        <div className="book-authors">{book.author}</div>
-
       </div>
-    )
-  }
+      <div className="book-title">{book.title}</div>
+      <div className="book-authors">{book.author}</div>
+
+    </div>
+  )
+}
+
+Book.propTypes = {
+  book: PropTypes.object.isRequired
 }
 
 export default Book
